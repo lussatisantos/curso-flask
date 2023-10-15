bb485e615463177b65b3d97b81de4ffb1b147509
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-15 18:54:56 +0100

    rotas dinamicas

--- 03/qpp.py
+++ 03/qpp.py
@@ -2,10 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello/')
 @app.route('/hello/<nome>')
 def hello(nome=''):
     return '<h1> Hello {}</h1>'.format(nome)
+
+@app.route('/blog/')
+@app.route('/blog/<int:postID>')
+def blog(postID=-1):
+    if postID >= 0:
+        return 'blog info {}'.format(postID)
+    else:
+        return 'blog todo'
+
+
+@app.route('/blog/<float:postID>')
+def blog2(postID=-1):
+    if postID >= 0:
+        return 'blog info {}'.format(postID)
+    else:
+        return 'blog todo'
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
