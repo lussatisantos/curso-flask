c4e78f6fb6812ad7b887c2ce2c5b73938e2fa7d9
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-15 19:08:55 +0100

    redirecionamento de url

--- 04/app.py
+++ 04/app.py
@@ -6,9 +6,16 @@ app = Flask(__name__)
 def admin():
     return '<h1>Admin</h1>'
 
-@app.route('/guest/<name>')
-def guest(name):
-    return '<p> ola guest <b>%s</b></p>' % name
+@app.route('/guest/<guest>')
+def guest(guest):
+    return '<p> ola guest <b>%s</b></p>' % guest
+
+@app.route('/user/<name>')
+def user(name):
+    if name == 'admin':
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('guest', guest = name))
 
 if __name__ == '__main__':
     app.run(debug=True)
