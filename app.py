f6f71f2234942b8a6357a32188570131177f2fcf
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-15 17:53:30 +0100

    .

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index'
+
+@app.route('/teste')
+def teste():
+    return 'teste'
+
+if __name__ == '__main__':
+    app.run()
